48730d4f61605488b2b7a61766767275c00658de
Natalie Brown <natalierawle@gmail.com>
2019-07-20 21:03:40 -0500

    corrected printing error

--- testhelper.go
+++ testhelper.go
@@ -4,6 +4,6 @@ import "testing"
 
 func assertEqual(t *testing.T, expected, actual interface{}, name string) {
 	if expected != actual {
-		t.Fatalf("\nexpected %s: %+v\nactual %s: %+v", expected, name, actual, name)
+		t.Fatalf("\nexpected %s: %+v\nactual %s: %+v", name, expected, name, actual)
 	}
 }
