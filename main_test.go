8b0001e238ac611659c1934b528570e5c2da181e
Natalie Brown <natalierawle@gmail.com>
2019-07-23 12:27:21 -0500

    cleaned up tests

--- main_test.go
+++ main_test.go
@@ -1,9 +1,17 @@
 package counter
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
+// assertEqual is a helper for the most common test assertion.
 func assertEqual(t *testing.T, expected, actual interface{}, name string) {
 	if expected != actual {
 		t.Fatalf("\nexpected %s: %+v\nactual %s: %+v", name, expected, name, actual)
 	}
+}
+
+func TestMain(m *testing.M) {
+	os.Exit(m.Run())
 }
